28c9615059fddca3a1a96bfa9242dd30cd3cd461
semicolonnn21 <iszulwilsa@uinssc.ac.id>
2026-03-03 14:33:22 +0700

    pertemuanke3

--- pertemuan.py
+++ pertemuan.py
@@ -1,3 +1,23 @@
-list = [1,2,3,4,5,6]
+list1 = [1,2,3]
+list2 = [2.1, 2.2, 2.3]
+list3 = ['a', 'b', 'c']
+listgabungan = [list1, list2, list3]
+print(list1)
+print(list2)
+print(list3)
+print(listgabungan)
 
-print(list)
+list_tipe_data = [100, 3.78, 'Caca Marica', True]
+a=[list_tipe_data[0], list_tipe_data[2]]
+print(a)
+
+warna = ['merah', 'hijau', 'kuning', 'biru', 'pink', 'ungu']
+warna2 = warna.copy()
+
+print("list setelah di copy")
+print(warna2)
+del warna2[4]
+print("list setelah di edit")
+print(warna2)
+print("list asli")
+print(warna)
